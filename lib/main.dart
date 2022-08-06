3d1b62745f849c99e0956767b682f6dbf19d6203
Petro Torskyy <x3mweb@gmail.com>
2022-08-06 19:51:08 +0300

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,17 @@ import 'package:ecommerce/blocs/wishlist/wishlist_bloc.dart';
 import 'package:ecommerce/config/app_router.dart';
 import 'package:ecommerce/config/theme.dart';
 import 'package:ecommerce/screens/screens.dart';
+import 'package:ecommerce/simple_bloc_observer.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'blocs/cart/cart_bloc.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  Bloc.observer = SimpleBlocObserver();
   runApp(const MyApp());
 }
 
