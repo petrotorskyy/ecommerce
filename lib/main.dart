d5c939768b6af7b3c7e02dac36ae39688babe3de
Petro Torskyy <x3mweb@gmail.com>
2022-08-06 19:15:06 +0300

    project ecom

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:ecommerce/blocs/wishlist/wishlist_bloc.dart';
+import 'package:ecommerce/config/app_router.dart';
+import 'package:ecommerce/config/theme.dart';
+import 'package:ecommerce/screens/screens.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'blocs/cart/cart_bloc.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => WishlistBloc()..add(StartWishlist())),
+        BlocProvider(create: (_) => CartBloc()..add(CartStarted())),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Ecommerce App',
+        theme: theme(),
+        onGenerateRoute: AppRouter.onGenerateRoute,
+        initialRoute: SplashScreen.routeName,
+        home: HomeScreen(),
+      ),
+    );
+  }
+}
